9ca06ba838da1196d390c610c934b2ed43207712
yusukesakamoto <yusukesakamoto>
2021-01-16 17:28:18 +0900

    nextだけ。ループどうする。

--- slider.js
+++ slider.js
@@ -9,13 +9,7 @@ let clonescore = 0;
 
 next.addEventListener('click',function(){
 
-if(slidescore === 3) {
-    slidescore = 0;
-}
-
-if(clonescore === 3) {
-    clonescore = 0;
-}
+slideall = document.querySelectorAll('#slider-container ul li');
 
 slidecount = slideall.length;
 slidecopy = slideall[clonescore].cloneNode(true);
@@ -23,4 +17,21 @@ slideul.appendChild(slidecopy);
 slideul.style.transform = "translateX(" + -420 * (slidescore) + "px)";
 slidescore++;
 clonescore++;
+
+console.log(slidescore);
+console.log(clonescore);
+
+
+
+if(slidescore === 4) {
+    slidescore = 1;
+    for(var i = 0;i < 3;i++){
+        console.log("動いてる");
+        slideall = document.querySelectorAll('#slider-container ul li');
+        slideall[0].remove();
+    }
+    slideul.style.transform = "translateX(" + 0 + "px)";
+    clonescore = 0;
+}
+
 },false);
