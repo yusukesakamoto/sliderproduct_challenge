c88874beb098aedfc09d452bacb328a9698eddc1
yusukesakamoto <yusukesakamoto>
2021-01-21 08:17:54 +0900

    一応の完成！

--- slider.js
+++ slider.js
@@ -1,59 +1,72 @@
-let slideall = document.querySelectorAll('#slider-container ul li');
+let slidecontainer = document.getElementById("slider-container");
+let slideall = document.querySelectorAll("#slider-container ul li");
 let slideul = document.querySelector("#slider-container ul");
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let slidesheet; // スライドの枚数
 let slidecount;
 let slidecopy;
 let slidescore = 1;
 let clonescore = 0;
 let distance = 0;
-
+let slidewidth = slidecontainer.clientWidth;
 let slideinterval;
 
-next.addEventListener('click',function(){
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    slidesheet = slideall.length;
+    slidecopy = slideall[clonescore].cloneNode(true);
+    slideul.appendChild(slidecopy);
+  },
+  false
+);
 
-slideall = document.querySelectorAll('#slider-container ul li');
+next.addEventListener(
+  "click",
+  function () {
+    slidewidth = slidecontainer.clientWidth;
 
-slideinterval =
-setInterval(() => {
-    if(distance < 420 * slidescore) {
+    slideall = document.querySelectorAll("#slider-container ul li");
 
-        distance = distance + 20;
+    slideinterval = setInterval(() => {
+      if (distance < slidewidth * slidescore) {
+        distance = distance + 30;
 
-        slideul.style.marginLeft = -(distance) + "px";
-    } else {
+        slideul.style.marginLeft = -distance + "px";
+      } else {
         clearInterval(slideinterval);
         slidescore++;
 
-        if(slidescore === 4) {
-            slidescore = 1;
-            slideul.style.marginLeft = 0 + "px";
-            distance = 0;
+        if (slidescore === slidesheet + 1) {
+          slidescore = 1;
+          slideul.style.marginLeft = 0 + "px";
+          distance = 0;
+        } else {
+        }
+      }
+    }, 10);
+  },
+  false
+);
+
+prev.addEventListener("click", function () {
+  slidewidth = slidecontainer.clientWidth;
+
+  if (slidescore === 1) {
+    distance = slidewidth * slidesheet;
+    slideul.style.marginLeft = -(slidesheet * slidewidth) + "px";
+    slidescore = slidesheet;
+  } else {
+    slidescore--;
+  }
+
+  slideinterval = setInterval(() => {
+    if (distance > slidewidth * (slidescore - 1)) {
+      distance = distance - 30;
+      slideul.style.marginLeft = -distance + "px";
     } else {
-        
+      clearInterval(slideinterval);
     }
-    }
-}, 5);
-
-},false);
-
-prev.addEventListener('click',function(){
-    if(slidescore === 1) {
-        distance = 420 * 3;
-        slideul.style.marginLeft = -(3 * 420) + "px";
-        slidescore = 3;
-    } else {
-        slidescore--;
-    }
-
-        slideinterval = setInterval(() => {
-            if(distance > 420 * (slidescore - 1 )) {
-                distance = distance - 20;
-                slideul.style.marginLeft = -(distance) + "px";
-            } else {
-                clearInterval(slideinterval);
-            }
-        }, 5); 
-    
-
+  }, 10);
 });
